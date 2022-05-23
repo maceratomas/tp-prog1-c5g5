8b4df0f7c88e12e39835e2d2d2233d81a2b88ea3
maceratomas <maceratomas@gmail.com>
2022-05-23 09:22:19 -0300

    Que Mikasa se mueva para rodear el obstáculo
    Que Mikasa se mueva en x o en y, para rodear el obstáculo.
    Por ahora solo se mueve en dos direcciones

--- src/juego/Mikasa.java
+++ src/juego/Mikasa.java
@@ -31,6 +31,32 @@ public class Mikasa {
 //		else
 //			entorno.dibujarImagen(img1, this.x, this.y, this.angulo, 0.2);
 	}
+	public boolean tocaObstaculoLadoDer(Obstaculos obstaculo, double valorEnCos, double valorEnSen) {
+		if((valorEnCos <= obstaculo.x+obstaculo.ancho+1.0) && (valorEnCos >= obstaculo.x+obstaculo.ancho-1)) {
+			return true;
+		}
+		return false;
+	}
+	public boolean tocaObstaculoLadoIzq(Obstaculos obstaculo, double valorEnCos, double valorEnSen) {
+		if((valorEnCos <= obstaculo.x+1.0) && (valorEnCos >= obstaculo.x-1)) {
+			return true;
+		}
+		return false;
+	}
+	public boolean tocaObstaculoLadoArriba(Obstaculos obstaculo, double valorEnCos, double valorEnSen) {
+		if((valorEnSen <= obstaculo.y-obstaculo.alto+1) && (valorEnSen >= obstaculo.y-obstaculo.alto-1)) {
+			return true;
+		}
+		return false;
+	}
+	public boolean tocaObstaculoLadoAbajo(Obstaculos obstaculo, double valorEnCos, double valorEnSen) {
+		// no se si efectivamente hay que restarle el alto
+		// ¿no se si dibuja desde arriba?
+		if((valorEnSen <= obstaculo.y+obstaculo.alto+1) && (valorEnSen >= obstaculo.y+obstaculo.alto-1)) {
+			return true;
+		}
+		return false;
+	}
 	public boolean tocaObstaculo(Obstaculos obstaculo, double valorEnCos, double valorEnSen) {
 		// primero ve si se choca en el eje x, luego lo hace con el eje y
 		if((valorEnCos >= obstaculo.x-obstaculo.ancho) && (valorEnCos <= (obstaculo.x + obstaculo.ancho))
@@ -57,6 +83,7 @@ public class Mikasa {
 	public void mover(int anchoPantalla, int alturaPantalla, double velocidad, Obstaculos obstaculo, Enemigos enemigo ) {
 		double valorEnCos = this.x + Math.cos(this.angulo)*velocidad;
 		double valorEnSen = this.y + Math.sin(this.angulo)*velocidad;
+		double moverAlTocarObj = 0.5;
 		/*
 		 * si el valorEn da un numero fuera de los bordes, no avanza -- Se traba con el
 		 * --> centro del objeto, no con los bordes del objeto -- Falta que se trabe tmb con
@@ -65,9 +92,27 @@ public class Mikasa {
 		
 		if ((valorEnCos-this.ancho/2 > 0 && valorEnSen-this.alto/2 > 0) && 
 			(valorEnCos+this.ancho/2 < anchoPantalla && valorEnSen+this.alto/2 < alturaPantalla)&& 
-			!(tocaObstaculo(obstaculo, valorEnCos, valorEnSen))&&!(tocaEnemigo(enemigo, valorEnCos, valorEnSen))) {
-			this.x = valorEnCos;
-			this.y = valorEnSen;
+			!(tocaEnemigo(enemigo, valorEnCos, valorEnSen))) {
+			if(tocaObstaculo(obstaculo, valorEnCos, valorEnSen)) {
+				//hay que rodear al objeto
+				// hago que suba solo en y, y en x para probar
+				if(tocaObstaculoLadoDer(obstaculo,valorEnCos,valorEnSen)) {
+					System.out.println("Toca el lado derecho");
+					this.y += moverAlTocarObj;
+				} else if(tocaObstaculoLadoIzq(obstaculo,valorEnCos,valorEnSen)) {
+					System.out.println("toca el lado izq");
+					this.y += moverAlTocarObj;
+				} else if (tocaObstaculoLadoArriba(obstaculo,valorEnCos,valorEnSen)) {
+					System.out.println("toca el lado arriba");
+					this.x += moverAlTocarObj;
+				} else {
+					System.out.println("toca el lado abajo");
+					this.x += moverAlTocarObj;
+				}
+			} else {
+				this.x = valorEnCos;
+				this.y = valorEnSen;
+			}
 		}
 	}
 	public void rotar() {
